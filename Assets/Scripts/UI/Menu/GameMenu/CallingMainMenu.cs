61836d99ebe2ea6273b2860ce1c0b3e69c8d23c7
Vitaliy-Todorov <65270620+Vitaliy-Todorov@users.noreply.github.com>
2022-11-04 01:27:28 +0300

    Random grid. CallingMainMenu

---
+++ Assets/Scripts/UI/Menu/GameMenu/CallingMainMenu.cs
@@ -0,0 +1,25 @@
+using Assets.Scripts.Infrastructure.System.InputSystem;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace UI.Menu
+{
+    public class CallingMainMenu : Button
+    {
+        private IInputSystem _inputSystem;
+        private GameMenuGeneral _gameMenuGeneral;
+
+        public void Construct(IInputSystem inputSystem, GameMenuGeneral gameMenuGeneral)
+        {
+            _inputSystem = inputSystem;
+            _gameMenuGeneral = gameMenuGeneral;
+        }
+        
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+            _gameMenuGeneral.EnableAndDisableMenu();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Menu/LevelMenu.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Assets.Scripts.UI.Menu
+{
+    public class LevelMenu : MonoBehaviour
+    {
+        
+    }
+}
